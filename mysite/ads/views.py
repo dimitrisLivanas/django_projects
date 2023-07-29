16ac5b4eb4f8fdfd30ce8191b6ce16e24ce6c3c7
dimitrisLivanas <livanas.dimitris@gmail.com>
2023-07-29 12:27:38 +0200

    Re-add the fields atributes on views.py

--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -14,12 +14,12 @@ class AdDetailView(OwnerDetailView):
 class AdCreateView(OwnerCreateView):
     model = Ad
     # List the fields to copy from the Ad model to the Ad form
-    # fields = ['title', 'text']
+    fields = ['title', 'text']
     fields_exclude = ['owner', 'created_at', 'updated_at']
 
 class AdUpdateView(OwnerUpdateView):
     model = Ad
-    # fields = ['title', 'text']
+    fields = ['title', 'text']
     # This would make more sense
     fields_exclude = ['owner', 'created_at', 'updated_at']
 
